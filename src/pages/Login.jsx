a5a45c4957a8b69d0164bef8584063954fe9054a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-09 22:40:43 +0100

    Set login status & display error

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import { loginUser } from "../api";
+import { useState } from "react";
 
 export function loader({ request }) {
     return new URL(request.url).searchParams.get('message');
@@ -9,11 +10,20 @@ export function loader({ request }) {
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" });
     const message = useLoaderData();
+    const [state, setState] = useState('idle');
+    const [error, setError] = useState(null)
 
     async function handleSubmit(e) {
         e.preventDefault()
-        const data = await loginUser(loginFormData);
-        console.log(data)
+        try {
+            setState('submitting')
+            const data = await loginUser(loginFormData);
+            console.log(data)
+        } catch (error) {
+            setError(error);
+        } finally {
+            setState('idle')
+        }
     }
 
     function handleChange(e) {
@@ -28,6 +38,8 @@ export default function Login() {
         <div className="login-container">
             <h1>Sign in to your account</h1>
             {message && <h3 className="error-message">{message}</h3>}
+            {error && <h3 className="error-message">{error.message}</h3>}
+
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -43,7 +55,7 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button disabled={state === 'submitting'}>Log in</button>
             </form>
         </div>
     )
