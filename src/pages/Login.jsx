d307c47d4aa6532f333c1b7834e2c530a15f171f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-09 23:12:35 +0100

    Setup form action for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Form } from "react-router-dom";
 import { loginUser } from "../api";
 import { useState } from "react";
 
@@ -7,33 +7,38 @@ export function loader({ request }) {
     return new URL(request.url).searchParams.get('message');
 }
 
+export async function action() {
+    console.log('Action completed');
+    return null
+}
+
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" });
+    // const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" });
     const message = useLoaderData();
     const [status, setStatus] = useState('idle');
     const [error, setError] = useState(null)
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-        try {
-            setStatus('submitting')
-            setError(null)
-            const data = await loginUser(loginFormData);
-            console.log(data)
-        } catch (error) {
-            setError(error);
-        } finally {
-            setStatus('idle')
-        }
-    }
+    // async function handleSubmit(e) {
+    //     e.preventDefault()
+    //     try {
+    //         setStatus('submitting')
+    //         setError(null)
+    //         const data = await loginUser(loginFormData);
+    //         console.log(data)
+    //     } catch (error) {
+    //         setError(error);
+    //     } finally {
+    //         setStatus('idle')
+    //     }
+    // }
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+    // function handleChange(e) {
+    //     const { name, value } = e.target
+    //     setLoginFormData(prev => ({
+    //         ...prev,
+    //         [name]: value
+    //     }))
+    // }
 
     return (
         <div className="login-container">
@@ -41,25 +46,21 @@ export default function Login() {
             {message && <h3 className="error-message">{message}</h3>}
             {error && <h3 className="error-message">{error.message}</h3>}
 
-            <form onSubmit={handleSubmit} className="login-form">
+            <Form method="post" className="login-form">
                 <input
                     name="email"
-                    onChange={handleChange}
                     type="email"
                     placeholder="Email address"
-                    value={loginFormData.email}
                 />
                 <input
                     name="password"
-                    onChange={handleChange}
                     type="password"
                     placeholder="Password"
-                    value={loginFormData.password}
                 />
                 <button disabled={status === 'submitting'}>
                     {status === 'submitting' ? 'Logging in...' : 'Log in'}
                 </button>
-            </form>
+            </Form>
         </div>
     )
 
