4e58208850a2ce31706c311e1b866bd59b0f57c9
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-13 11:28:31 +0100

    Update Dashboard page loader function

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans, { loader as vansLoader } from "./pages/Vans/Vans"
 import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail"
-import Dashboard from "./pages/Host/Dashboard"
+import Dashboard, {loader as vanListLoader} from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
 import HostVans, { loader as hostVansLoader } from "./pages/Host/HostVans"
@@ -52,7 +52,7 @@ const router = createBrowserRouter(createRoutesFromElements(
             <Route
                 index
                 element={<Dashboard />}
-                loader={async ({request}) => await requireAuth(request)} />
+                loader={vanListLoader} />
             <Route
                 path="income"
                 element={<Income />}
